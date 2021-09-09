9dbcf442825e47ba6b81ca542473c86aff3a1089
luisluft <luberluft@gmail.com>
2021-09-09 08:18:29 -0400

    add book component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,29 @@ import React from "react";
 import ReactDom from "react-dom";
 
 // Needs to be capitalized so that react can understand this is a react component
-function Greeting() {
-  return <h4>this is Luis, and this is my first component</h4>;
+// can only return a single element (wrap everything inside a React.Fragment)
+// use camelCase for html attributes (className)
+function BookList() {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = () => (
+  <book>
+    <Image />
+    <Title />
+    <Author />
+  </book>
+);
+
+const Image = () => (
+  <img src="https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg"></img>
+);
+
+const Author = () => <h4>Ann Whitford Paul</h4>;
+
+const Title = () => <h1>If Animals Kissed Good Night</h1>;
+ReactDom.render(<BookList />, document.getElementById("root"));
