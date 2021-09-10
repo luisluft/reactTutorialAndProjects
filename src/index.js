330b35a0fd2517811a87d9d702d33e42bdeb60dc
luisluft <luberluft@gmail.com>
2021-09-10 09:38:11 -0400

    add event handlers to the component

--- src/index.js
+++ src/index.js
@@ -43,11 +43,22 @@ function BookList() {
 }
 
 const Book = ({ imageUrl, author, title }) => {
+  const clickHandler = (author) => {
+    console.log(author);
+  };
+
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}>
       <img alt="my face" src={imageUrl}></img>
       <h1>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={() => clickHandler(author)}>
+        click me
+      </button>
     </article>
   );
 };
