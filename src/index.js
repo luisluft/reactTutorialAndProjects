0cdda98de3936334e53415243efe2487e3ec684f
luisluft <luberluft@gmail.com>
2021-09-10 09:28:06 -0400

    refactor books into a single array of objects and use as props inside the component

--- src/index.js
+++ src/index.js
@@ -2,18 +2,30 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-const firstBook = {
-  imageUrl:
-    "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg",
-  title: "If Animals Kissed Good Night",
-  author: "Ann Whitford Paul",
-};
-const secondBook = {
-  imageUrl:
-    "	https://images-na.ssl-images-amazon.com/images/I/91Y0MgtvzZL._AC_UL200_SR200,200_.jpg",
-  title: "The Hawthorne Legacy (The Inheritance Games, 2)",
-  author: "Jennifer Lynn Barnes",
-};
+const books = [
+  {
+    id: 1,
+    imageUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg",
+    title: "If Animals Kissed Good Night",
+    author: "Ann Whitford Paul",
+  },
+  {
+    id: 2,
+    imageUrl:
+      "	https://images-na.ssl-images-amazon.com/images/I/91Y0MgtvzZL._AC_UL200_SR200,200_.jpg",
+    title: "The Hawthorne Legacy (The Inheritance Games, 2)",
+    author: "Jennifer Lynn Barnes",
+  },
+  {
+    id: 3,
+    imageUrl:
+      "	https://images-na.ssl-images-amazon.com/images/I/81icCFFuGUS._AC_UL200_SR200,200_.jpg",
+    title:
+      "Countdown bin Laden: The Untold Story of the 247-Day Hunt to Bring the Mastermind of 9/11 to Justice (Chris Wallace’s…",
+    author: "Chris Wallace",
+  },
+];
 
 // Needs to be capitalized so that react can understand this is a react component
 // can only return a single element (wrap everything inside a React.Fragment)
@@ -21,34 +33,23 @@ const secondBook = {
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        imageUrl={firstBook.imageUrl}
-        title={firstBook.title}
-        author={firstBook.author}>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
-          provident! Quaerat reiciendis debitis consequuntur saepe quae! Aliquid
-          ipsam vitae, sit, rem dolorem nesciunt tenetur voluptas corporis quae
-          odit sequi? Dignissimos.
-        </p>
-      </Book>
-      <Book
-        imageUrl={secondBook.imageUrl}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        const { imageUrl, title, author } = book;
+
+        return <Book key={book.id} book={book}></Book>;
+      })}{" "}
     </section>
   );
 }
 
-const Book = ({ imageUrl, title, author, children }) => {
+const Book = (props) => {
+  const { imageUrl, author, title } = props.book;
   return (
-    <book className="book">
+    <article className="book">
       <img alt="my face" src={imageUrl}></img>
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
-    </book>
+    </article>
   );
 };
 
